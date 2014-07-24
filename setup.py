b0c0928540d55f1aa9b93fe0000c9e79eb1fc16e
r00tl3ss <domenik.jones@gmail.com>
2014-07-24 06:32:25 +0200

    file references in setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     author_email='domenik.jones.gmail.com',
     packages=['like_system'],
     url='https://github.com/r00tl3ss/django-like-system.git',
-    license='BSD licence, see LICENCE.txt',
+    license='BSD licence, see LICENCE.rst',
     description='Django based like system. Inspired by django.contrib.comments.',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rm').read(),
     zip_safe=False,
 )
