f10195d961c64c0d5c8847308271d33ecacf0391
r00tl3ss <domenik.jones@netzbarkeit.ch>
2014-07-24 03:13:05 +0200

    init commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='django-like-syste',
+    version='0.0.1',
+    author=u'Domenik Jones',
+    author_email='domenik.jones.gmail.com',
+    packages=['like_system'],
+    url='https://github.com/r00tl3ss/django-like-system.git',
+    license='BSD licence, see LICENCE.txt',
+    description='Django based like system. Inspired by django.contrib.comments.',
+    long_description=open('README.txt').read(),
+    zip_safe=False,
+)
