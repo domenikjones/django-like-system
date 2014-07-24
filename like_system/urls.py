8f49d626c019548f391e8e85e3f3697069f5148b
r00tl3ss <domenik.jones@gmail.com>
2014-07-24 06:11:05 +0200

    urls and views

--- like_system/urls.py
+++ like_system/urls.py
@@ -1,1 +1,7 @@
-__author__ = 'djones'
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns('like_system.views',
+    url(r'unlike/(?P<content_type>\w+)/(?P<object_pk>[0-9]+)', 'unlike', name='like_system-unlike'),
+    url(r'like/(?P<content_type>\w+)/(?P<object_pk>[0-9]+)', 'like', name='like_system-like'),
+)
--- like_system/views.py
+++ like_system/views.py
@@ -1,1 +1,48 @@
-__author__ = 'djones'
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.sites.models import get_current_site
+from django.http.response import HttpResponse
+
+from like_system.models import Like
+
+
+def like(request, content_type=None, object_pk=None):
+    # validate the url parameters
+    try:
+        ct = ContentType.objects.get(model=content_type)
+        site = get_current_site(request)
+    except:
+        return HttpResponse(False)
+
+    # make this unique
+    try:
+        like = Like.objects.get(user=request.user,
+                                content_type=ct,
+                                site=site,
+                                object_pk=object_pk
+        )
+    except:
+        like = Like(user=request.user,
+                    content_type=ct,
+                    site=site,
+                    object_pk=object_pk,
+        )
+        like.save()
+
+    return HttpResponse(like)
+
+
+def unlike(request, content_type=None, object_pk=None):
+    # validate the url parameters
+    try:
+        ct = ContentType.objects.get(model=content_type)
+        site = get_current_site(request)
+        # delete unique like
+        Like.objects.get(user=request.user,
+                         content_type=ct,
+                         site=site,
+                         object_pk=object_pk
+        ).delete()
+    except:
+        return HttpResponse(False)
+
+    return HttpResponse(True)
