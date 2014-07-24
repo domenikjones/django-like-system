ed65438ef28d5b905d1fbdcbd013be8fc10e9b70
r00tl3ss <domenik.jones@gmail.com>
2014-07-24 06:10:47 +0200

    admin

--- like_system/admin.py
+++ like_system/admin.py
@@ -1,1 +1,10 @@
-__author__ = 'djones'
+from django.contrib import admin
+from like_system.models import Like
+
+
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('user', 'content_type', 'object_pk', )
+    list_filter = ('content_type', )
+    raw_id_fields = ('user', )
+
+admin.site.register(Like, LikeAdmin)
