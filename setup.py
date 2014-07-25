26179b3aa54f7af0b79530efad9d03b4565a30d7
r00tl3ss <domenik.jones@gmail.com>
2014-07-25 12:41:37 +0200

    README.rst

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='django-like-system',
-    version='0.0.1-alpha1',
+    version='0.0.1-alpha2',
     author=u'Domenik Jones',
     author_email='domenik.jones.gmail.com',
     packages=['like_system'],
     url='https://github.com/r00tl3ss/django-like-system.git',
     license='BSD licence, see LICENCE.rst',
     description='Django based like system. Inspired by django.contrib.comments.',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
 )
