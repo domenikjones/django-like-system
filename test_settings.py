221f00e1a7c0761c5d972c456604da862689c361
r00tl3ss <domenik.jones@gmail.com>
2014-07-25 12:32:42 +0200

    travis-ci support

--- test_settings.py
+++ test_settings.py
@@ -1,24 +1,47 @@
-import os
+# -*- coding: utf-8 -*-
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+
+import os, sys
+PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.join(PROJECT_DIR, 'like_system/testapp'))
+
+SECRET_KEY = 'django-like-systemTEST$3cr3t_KEY'
+SITE_ID = 1
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
-ROOT_URLCONF = 'likes.urls'
+ROOT_URLCONF = 'like_system.urls'
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.request",
+    "django.contrib.messages.context_processors.messages",
+)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
-    'django.contrib.admin',
     'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.admin',
+
     'like_system',
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
+STATIC_ROOT = '/tmp/'  # Dummy
+STATIC_URL = '/static/'
